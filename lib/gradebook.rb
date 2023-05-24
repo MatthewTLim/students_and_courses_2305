535878aceb6963d849ed33e66f51cd16702b4b35
Matthew Lim <matlim200@gmail.com>
2023-05-24 09:29:33 -0700

    Add gradebook class and methods #add_course, #list_all_students, and #students_below

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,33 @@
 class Gradebook
-  
+
+  attr_reader :instructor, :courses
+
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+    all_students = {}
+    @courses.each do |course|
+      all_students[course.name] = course.students
+    end
+    all_students
+  end
+
+  def students_below(threshold)
+    below = []
+     @courses.each do |course|
+      course.students.each do |student|
+        if student.grade < threshold
+          below << student
+        end
+      end
+    end
+    below
+  end
 end
