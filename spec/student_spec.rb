0d7355ad0cbc2c2a3a4e5b0c101ee1f369843e9f
Matthew Lim <matlim200@gmail.com>
2023-05-24 08:07:48 -0700

    Initital commit

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,39 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+  it "exists" do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student).to be_a(Student)
+  end
+
+  it "has readbale attributes" do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student.name).to eq("Morgan")
+    expect(student.age).to eq(21)
+    expect(student.scores).to eq([])
+  end
+
+  describe "#log_score" do
+    it "can log the students score" do
+      student = Student.new({name: "Morgan", age: 21})
+
+      student.log_score(89)
+      student.log_score(78)
+
+      expect(student.scores).to eq([89, 78])
+    end
+  end
+
+  describe "#grade" do
+    it "can grade the student by finding the average of all their scores" do
+      student = Student.new({name: "Morgan", age: 21})
+
+      student.log_score(89)
+      student.log_score(78)
+
+      expect(student.grade).to eq(83.5)
+    end
+  end
 end
